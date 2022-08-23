986e1b25b292a6ad54bee6b98fdf45f4a5f88b03
Giovane Souza <giovanebolsoni2@gmail.com>
2022-08-22 22:45:57 -0300

    Building the header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+                <link rel="shortcut icon" href="/favicon.png" type="image/png" />
             </Head>
             <body>
                 <Main />
